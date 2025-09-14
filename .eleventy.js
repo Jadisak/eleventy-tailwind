461508cb2aea74f162bae2d6056340a8869ef339
Adisak Jantra Sopark <ajsopark@gmail.com>
2025-09-14 17:54:24 +0700

    Test 1
    ssszzz

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,12 @@
 module.exports = function(eleventyConfig) {
    eleventyConfig.addPassthroughCopy("assets");
 }
+{
+  return {
+    // ...existing config...
+    browserSync: {
+      host: "0.0.0.0",
+      port: 8081
+    }
+  };
+};
